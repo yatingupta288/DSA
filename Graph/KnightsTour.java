aff0017b107ca736b4399604c3674da1f7457e9a
yatingupta288 <yatinrockz288@gmail.com>
2021-05-15 00:11:08 +0530

    Graph Problem 9

---
+++ Graph/KnightsTour.java
@@ -0,0 +1,20 @@
+public static void printKnightsTour(int[][] chess, int r, int c, int upcomingMove) {
+        if(r < 0 || c < 0 || r >= chess.length || c >= chess.length || chess[r][c] > 0){
+            return;
+        }else if(upcomingMove == chess.length * chess.length){
+            chess[r][c] = upcomingMove;
+            displayBoard(chess);
+            chess[r][c] = 0;
+            return;
+        }
+        chess[r][c] = upcomingMove;
+        printKnightsTour(chess, r - 2, c + 1, upcomingMove + 1);
+        printKnightsTour(chess, r - 1, c + 2, upcomingMove + 1);
+        printKnightsTour(chess, r + 1, c + 2, upcomingMove + 1);
+        printKnightsTour(chess, r + 2, c + 1, upcomingMove + 1);
+        printKnightsTour(chess, r + 2, c - 1, upcomingMove + 1);
+        printKnightsTour(chess, r + 1, c - 2, upcomingMove + 1);
+        printKnightsTour(chess, r - 1, c - 2, upcomingMove + 1);
+        printKnightsTour(chess, r - 2, c - 1, upcomingMove + 1);
+        chess[r][c] = 0;
+    }
