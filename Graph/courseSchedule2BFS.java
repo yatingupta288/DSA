7e438b6bff87fc30e200bd4b010dea5963dd987a
yatingupta288 <yatinrockz288@gmail.com>
2021-05-21 17:17:26 +0530

    Graph problem 24

---
+++ Graph/courseSchedule2BFS.java
@@ -0,0 +1,30 @@
+class Solution {
+   public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] degree = new int[numCourses], res = new int[numCourses];
+        List<Integer>[] graph = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) graph[i] = new ArrayList<>();
+        for (int i = 0; i < prerequisites.length; i++) {
+            degree[prerequisites[i][0]]++;
+            graph[prerequisites[i][1]].add(prerequisites[i][0]);
+        }
+        int index = 0;
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (degree[i] == 0) {
+                q.offer(i);
+                res[index++] = i;
+                System.out.print(res + " ");
+            }
+        }
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            for (int nei : graph[cur]) {
+                if (--degree[nei] == 0) {
+                    q.offer(nei);
+                    res[index++] = nei;
+                }
+            }
+        }
+        return index == numCourses ? res : new int[0];
+    }
+}
