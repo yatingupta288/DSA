0662a15fa9ecca2adb669b9f9a3bab116c518fa6
yatingupta288 <yatinrockz288@gmail.com>
2021-05-28 13:51:16 +0530

    dp problem 1

--- Dp/targetSum.java
+++ Dp/targetSum.java
@@ -31,3 +31,7 @@ class Solution {
         return dp[n][diff];
     }
 }
+
+//Cnt is for '0's in the array. If we have n number of zeroes then we need to multiply out result of count of subsets with a sum to 2^n.  And we need to not consider this while calculating in bottom-up manner, hence the case " if(nums[i-1]==0) { dp[i][j] = dp[i-1][j] } ".
+//For example: If we have 2(cnt=2) zeroes in the example array for which we calculate count of subset with a sum then there are 4(2^cnt) ways that is either selecting first zero, second selecting second 0 present in array, third not selecting both, fourth selecting both.
+//Hope this will be helpful.
