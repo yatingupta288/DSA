e1866cc1e22509eca3f2e12cd08c13277baff495
yatingupta288 <yatinrockz288@gmail.com>
2021-06-02 15:23:21 +0530

    problem 6

--- Dp/interLeavingStrings.java
+++ Dp/interLeavingStrings.java
@@ -45,3 +45,30 @@ class Solution {
 //         return dfs(s1, s2, s3, 0, 0, 0, visited);
 //     }
 // }
+
+DP SOL.
+// class Solution {
+    
+//         public boolean isInterleave(String s1, String s2, String s3) {
+//         int m = s1.length(), n = s2.length();
+//         if (m + n != s3.length()) return false;
+
+//         // initializaion
+//         boolean[][] dp = new boolean[m + 1][n + 1];
+//         dp[0][0] = true;
+
+//         // function
+//         for (int i = 0; i <= m; i++) {
+//             for (int j = 0; j <= n; j++) {
+//                 if (i > 0 && s1.charAt(i - 1) == s3.charAt(i + j - 1)) {
+//                     dp[i][j] |= dp[i - 1][j];
+//                 }
+//                 if (j > 0 && s2.charAt(j - 1) == s3.charAt(i + j - 1)) {
+//                     dp[i][j] |= dp[i][j - 1];
+//                 }
+//             }
+//         }
+
+//         return dp[m][n];
+//     }
+// }
