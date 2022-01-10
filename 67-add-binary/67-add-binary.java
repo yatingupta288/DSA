24a4a21d39bcc31f02995c43fbbd1173009d68b2
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-01-11 02:34:17 +0530

    Time: 41 ms (5.24%), Space: 40.6 MB (5.29%) - LeetHub

---
+++ 67-add-binary/67-add-binary.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder s = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0){
+            int sum = 0;
+            if(i >= 0)sum += (a.charAt(i--) - '0');
+            if(j >= 0)sum += (b.charAt(j--) - '0');
+            sum += carry;
+            if(sum > 1){
+                carry = 1;
+            }else{
+                carry = 0;
+            }
+            s.append(sum%2);
+            System.out.print(sum + " ");
+        }
+        if(carry!= 0)s.append(carry);
+        return s.reverse().toString();
+    }
+}
