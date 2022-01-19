78abb3c3a3e755d21be6adeb1774eebe18c79e1f
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-01-19 11:31:16 +0530

    Time: 1 ms (51.28%), Space: 41.3 MB (36.30%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
@@ -3,14 +3,8 @@ class Solution {
         int i = 0;
         int count = 0;
         for(int n : nums){
-            count++;
-            if(i == 0 || n > nums[i - 1]){
+            if(i < 2 || n > nums[i - 2]){
                 nums[i++] = n;
-                count= 1;
-            }
-            if(count == 2){
-                nums[i] = nums[i - 1];
-                i++;
             }
         }
         return i;
