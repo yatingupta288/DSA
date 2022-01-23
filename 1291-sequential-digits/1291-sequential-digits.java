6cfe80ed4c740ce296a6d44fb73a2041969a7ef9
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-01-23 19:37:07 +0530

    Time: 1 ms (41.30%), Space: 38.3 MB (22.83%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -1,20 +1,22 @@
 class Solution {
-        public List<Integer> sequentialDigits(int low, int high) {
-            List<Integer> rv = new ArrayList<Integer>();
-            int start_length = (int)Math.log10(low);
-            int end_length = (int)Math.log10(high)+2;
-            for (int length = start_length; length < end_length; length++) {
-                for (int start = 1; start + length <= 10; start++) {
-                    int number = start;
-                    for (int len = 1; len < length; len++) {
-                        number *= 10;
-                        number += (len + start);
-                    }
-                    if (number >= low && number <= high) {
-                        rv.add(number);
-                    }
+    public List<Integer> sequentialDigits(int low, int high) {
+        int number = 0;
+        List<Integer> list = new ArrayList<>();
+        for(int digit = 1; digit < 9; digit++){ // first digit range
+            number = digit;
+            int next = number; 
+            while(number <= high && next < 10){
+                if(number >= low){
+                    list.add(number);
                 }
+                number = number * 10  + ++next;
             }
-            return rv;
         }
+        Collections.sort(list);
+        return list;
     }
+}
+
+// 1. find the first digit of the numbers
+// 2. iterate the number and add the digits
+// 3. 
