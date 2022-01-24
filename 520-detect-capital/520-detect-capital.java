53cef2626c052b55763eed96539455592b19c40d
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-01-24 15:04:56 +0530

    Time: 2 ms (59.30%), Space: 39.1 MB (34.60%) - LeetHub

--- 520-detect-capital/520-detect-capital.java
+++ 520-detect-capital/520-detect-capital.java
@@ -1,7 +1,7 @@
 public class Solution {
     public boolean detectCapitalUse(String word) {
-        int cnt = 0;
-        for(char c: word.toCharArray()) if('Z' - c >= 0) cnt++;
-        return ((cnt==0 || cnt==word.length()) || (cnt==1 && 'Z' - word.charAt(0)>=0));
+        int count = 0;
+        for(char ch : word.toCharArray())if('Z' - ch >=0)count++;
+        return (count == 0 || count == word.length() || (count == 1 && ('Z' - word.charAt(0) >= 0)));
     }
 }
