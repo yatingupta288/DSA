eca69187057b75218b44af68915d0646c1a24d92
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-01-26 22:53:27 +0530

    Time: 32 ms (27.59%), Space: 86.3 MB (28.59%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> list = new ArrayList<>();
+        helper(root1, list);
+        helper(root2, list);
+        Collections.sort(list);
+        return list;
+    }
+    
+    public void helper(TreeNode root, List<Integer> list){
+        if(root == null)return;
+        helper(root.left, list);
+        list.add(root.val);
+        helper(root.right, list);
+        return;
+    }
+}
