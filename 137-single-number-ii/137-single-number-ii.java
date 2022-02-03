2bc4063c6723bc5d4aad63725530742df85fd83b
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-02-04 01:42:40 +0530

    Time: 1 ms (89.90%), Space: 43.6 MB (11.25%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int x1 = 0;
+        int x2 = 0;
+        for(int i : nums){
+            x2 ^= (x1 & i);
+            x1 ^= i;
+            
+            int mask = ~(x1 & x2);
+            
+            x2 &= mask;
+            x1 &= mask;
+        }
+        
+        return x1 | x2;
+    }
+}
