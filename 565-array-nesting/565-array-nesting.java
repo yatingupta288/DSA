4cbf7a066d0499df76a341f9e9aaa4ea7401c3b7
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-02-07 01:53:35 +0530

    Time: 10 ms (47.64%), Space: 76.1 MB (61.22%) - LeetHub

--- 565-array-nesting/565-array-nesting.java
+++ 565-array-nesting/565-array-nesting.java
@@ -1,31 +1,18 @@
-// index of the next place
-// visited array
-// count to store the max length
 class Solution {
-    int res = 0;
     public int arrayNesting(int[] nums) {
-        int seen[] = new int[nums.length];
-        int idx = 0;
+        boolean seen[] = new boolean[nums.length];
         int count = 0;
+        int res = 0;
         for(int i = 0; i < nums.length; i++){
-            if(seen[i] != -1){
-                dfs(nums, i, seen, count);
+            while(seen[i] != true){
+                seen[i] = true;
+                count++;
+                i = nums[i];
             }
-        }
-
-        return res;
-    }
-    
-    public void dfs(int[] nums, int idx, int[] seen, int count){
-        // System.out.print(nums[idx] + " ");
-        if(seen[idx] == -1){
             res = Math.max(res, count);
-            return;
+            count = 0;
         }
         
-        seen[idx] = -1;
-        dfs(nums, nums[idx], seen, count + 1);
-        // seen[idx] = 0;
-        return;
+        return res;
     }
 }
