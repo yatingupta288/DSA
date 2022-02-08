96a946c969f34766c889ba465a700e0959a56023
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-02-08 09:05:10 +0530

    Time: 2 ms (59.15%), Space: 40.6 MB (14.72%) - LeetHub

--- 258-add-digits/258-add-digits.java
+++ 258-add-digits/258-add-digits.java
@@ -1,21 +1,6 @@
 class Solution {
     public int addDigits(int num) {
-        String s = sumOfDigits(num);
-        System.out.print(s + " ");
-        if(s.length() == 1){
-            return Integer.parseInt(s);
-        }else{
-            return addDigits(Integer.parseInt(s));
-        }
-    }
-    
-    public String sumOfDigits(int num){
-        int sum = 0;
-        while(num > 0){
-            sum += (num % 10);
-            num = num/10;
-        }
-        
-        return sum + "";
+        if(num == 0)return 0;
+        return num % 9 == 0 ? 9 : num % 9; 
     }
 }
