082c43a90fc43c99753c8a2a948d572df585e048
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-02-13 14:03:47 +0530

    Time: 0 ms (100.00%), Space: 42 MB (25.40%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,21 +1,22 @@
 class Solution {
+    List<List<Integer>> list = new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> res = new ArrayList<>();
-        helper(list, res, 0, nums);
+        ArrayList<Integer> sublist = new ArrayList<>();
+        sets(sublist,nums,0);
         return list;
     }
     
-    public void helper(List<List<Integer>> list, List<Integer> res, int j, int[] nums){
-        list.add(new ArrayList<>(res));
-        if(j == nums.length)return;
-        for(int i = j; i < nums.length; i++){
-            res.add(nums[i]);
-            //System.out.print(res);
-            helper(list, res, i + 1, nums);
-            res.remove(res.size() - 1);
+    public void sets(ArrayList<Integer> slist, int[] nums, int idx){
+        list.add(new ArrayList<>(slist));
+        // slist.add(nums[idx]);
+        if(idx == nums.length){
+            return;
+        } 
+        for(int i = idx; i < nums.length; i++){
+            slist.add(nums[i]);
+            sets(slist, nums, i + 1);
+            slist.remove(slist.size() - 1);
         }
-        
         return;
     }
 }
