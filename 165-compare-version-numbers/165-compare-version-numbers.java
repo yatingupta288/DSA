4ea7ab2dde9f3b09e05fcf44dc27e62b8a9f1188
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-02-26 09:53:57 +0530

    Time: 1 ms (90.92%), Space: 42.4 MB (12.05%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -2,18 +2,34 @@ class Solution {
     public int compareVersion(String version1, String version2) {
         String v1[] = version1.split("\\.");
         String v2[] = version2.split("\\.");
+        int i = 0;
+        int j = 0;
         
-        int n = v1.length > v2.length ? v1.length : v2.length;
-        
-        for(int i = 0; i < n; i++){
-            Integer a = i <v1.length ? Integer.parseInt(v1[i]):0;
-            Integer b = i <v2.length ? Integer.parseInt(v2[i]):0;
-            int val = a.compareTo(b);
-            if(val != 0){
-                return val;
+        int num1 = 0;
+        int num2 = 0;
+        int n = Math.max(v1.length, v2.length);
+        while(i < n){
+            while(i < v1.length && j < v1[i].length()){
+                num1 = num1 * 10 + v1[i].charAt(j) - '0';
+                j++;
+            }
+            j = 0;
+            while(i < v2.length && j < v2[i].length()){
+                num2 = num2 * 10 + v2[i].charAt(j) - '0';
+                j++;
+            }
+            j = 0;
+            i++;
+            if(num1 > num2){
+                return 1;
+            }else if(num1 < num2){
+                return -1;
+            }else{
+                num1 = 0;
+                num2 = 0;
+                continue;
             }
         }
-        
         return 0;
     }
 }
