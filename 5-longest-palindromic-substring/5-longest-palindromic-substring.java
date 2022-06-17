af44636043381156aa90e71064f6b6dbc2d2bac3
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-06-18 00:08:31 +0530

    Time: 1263 ms (5.12%), Space: 460.8 MB (5.01%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,27 +1,32 @@
+// diagonal traversal or gap strategy involved.
+// refer to pepcoding video for idea.
 class Solution {
     public String longestPalindrome(String s) {
-        boolean dp[][] = new boolean [s.length()][s.length()];
         String str = "";
-        for(int i = 0; i < s.length(); i++){
-            for(int k = 0, j = i; j < s.length(); j++, k++){
-                if(i == 0){
+        boolean dp[][] = new boolean[s.length()][s.length()];
+        for(int gap = 0; gap < s.length(); gap++){
+            for(int k = 0, j = gap; j < s.length(); j++, k++){
+                if(gap == 0){
                     dp[k][j] = true;
-                }else if(i == 1){
+                }else if( gap == 1){
                     if(s.charAt(k) == s.charAt(j)){
                         dp[k][j] = true;
                     }else{
                         dp[k][j] = false;
                     }
                 }else{
-                    if(s.charAt(k) == s.charAt(j) && dp[k + 1][j - 1] == true){
+                    if(s.charAt(k) == s.charAt(j) && dp[k + 1][j - 1]){
                         dp[k][j] = true;
+                    }else{
+                        dp[k][j] = false;
                     }
                 }
-                if(dp[k][j] == true){
+                if(dp[k][j]){
                     str = s.substring(k, j + 1);
                 }
             }
         }
+        
         return str;
     }
 }
