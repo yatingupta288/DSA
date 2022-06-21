fcce18274a476a3a4b8358b02d23bb440cc6ec0a
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-06-21 23:02:10 +0530

    Time: 9 ms (31.82%), Space: 44.3 MB (76.88%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,25 +1,25 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] c, int target) {
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        helper(list, temp, target, 0, c);
-        return list;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        int n = candidates.length;
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        helper(0, candidates, target, list, 0, n, res);
+        return res;
     }
     
-    public void helper(List<List<Integer>> list, List<Integer> temp, int remain, int start, int[] c){
-        if(remain < 0){
+    public void helper(int i, int[] arr, int target, List<Integer> list, int sum, int n, List<List<Integer>> res){
+        if(i == n)return;
+        if(sum > target)return;
+        if(sum == target){
+            res.add(new ArrayList<>(list));
             return;
         }
-        
-        if(remain == 0){
-            list.add(new ArrayList<>(temp));
-            return;
-        }
-        for(int i = start; i < c.length; i++){
-            temp.add(c[i]);
-            helper(list, temp, remain - c[i], i, c);
-            temp.remove(temp.size() - 1);
-        }
+        list.add(arr[i]);
+        sum += arr[i];
+        helper(i, arr, target, list, sum, n, res);
+        list.remove(list.size() - 1);
+        sum -= arr[i];
+        helper(i + 1, arr, target, list, sum, n, res);
         return;
     }
 }
