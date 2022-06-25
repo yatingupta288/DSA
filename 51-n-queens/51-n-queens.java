55be06f41b605209c19dfd930348ce8e2886943f
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-06-25 15:43:43 +0530

    Time: 2 ms (97.71%), Space: 42.7 MB (87.82%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -5,61 +5,70 @@ class Solution {
             for (int j = 0; j < n; j++)
                 seen[i][j] = '.';
         List<List<String>> list = new ArrayList<>();
-        solve(0,n, seen, list);
+        int col[] = new int[n];
+        int updg[] = new int[2 * n - 1];
+        int downdg[] = new int[2 * n - 1];
+        solve(0,n, seen, list, col, updg, downdg);
         return list;
     }
     
-    public void solve(int j, int n, char[][] seen, List<List<String>> list){
+    public void solve(int j, int n, char[][] seen, List<List<String>> list, int[] col, int[] updg, int[] downdg){
         if(j == n ){
             add(list, seen);
             return;
         }
         
         for(int i = 0; i < n; i++){
-            if(canFill(i,j,seen,n)){
+            if(col[i] == 0 && updg[i + j] == 0 && downdg[n - 1 + i - j] == 0){
+                col[i] = 1;
+                updg[i + j] = 1;
+                downdg[n - 1 + i - j] = 1;
                 seen[i][j] = 'Q';
-                solve(j + 1, n, seen, list);
+                solve(j + 1, n, seen, list, col, updg, downdg);
                 seen[i][j] = '.';
+                col[i] = 0;
+                updg[i + j] = 0;
+                downdg[n - 1 + i - j] = 0;
             }
         }
     }
     
-    public boolean canFill(int i, int j, char seen[][], int n){
-        int duprow = i;
-        int dupcol = j;
+//     public boolean canFill(int i, int j, char seen[][], int n){
+//         int duprow = i;
+//         int dupcol = j;
         
-        while(duprow >= 0 && dupcol >= 0){
-            if(seen[duprow][dupcol] == 'Q'){
-                return false;
-            }
-            duprow--;
-            dupcol--;
-        }
+//         while(duprow >= 0 && dupcol >= 0){
+//             if(seen[duprow][dupcol] == 'Q'){
+//                 return false;
+//             }
+//             duprow--;
+//             dupcol--;
+//         }
         
-        int row = i;
-        int col = j;
+//         int row = i;
+//         int col = j;
         
-        while(col >= 0){
-            if(seen[row][col] == 'Q'){
-                return false;
-            }
-            col--;
-        }
+//         while(col >= 0){
+//             if(seen[row][col] == 'Q'){
+//                 return false;
+//             }
+//             col--;
+//         }
         
-        row = i;
-        col = j;
+//         row = i;
+//         col = j;
         
-        while(row < n && col >= 0){
-            if(seen[row][col] == 'Q'){
-                return false;
-            }
-            row++;
-            col--;
-        }
+//         while(row < n && col >= 0){
+//             if(seen[row][col] == 'Q'){
+//                 return false;
+//             }
+//             row++;
+//             col--;
+//         }
         
-        return true;
+//         return true;
     
-    }
+//     }
     
     public void add(List<List<String>> res, char[][] seen){
         List<String> list = new ArrayList<>();
