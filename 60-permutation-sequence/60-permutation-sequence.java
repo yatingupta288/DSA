5620a851f4750eb0cca5be3d90cbc66ca165011a
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-06-29 23:23:32 +0530

    Time: 3 ms (67.03%), Space: 42 MB (33.14%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+        int fact = 1;
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 1; i < n; i++){
+            fact = fact * i;
+            arr.add(i);
+        }
+        arr.add(n);
+        String ans = "";
+        k = k - 1;
+        while(true){
+            ans = ans + arr.get(k/fact);
+            arr.remove(k/fact);
+            if(arr.size() == 0){
+                break;
+            }
+            k = k%fact;
+            fact = fact/arr.size();
+        }
+        
+        return ans;
+    }
+}
