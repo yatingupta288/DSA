5167fa1ff5053557dc8b0656aeb6cb66f75032bc
yatingupta288 <64730994+yatingupta288@users.noreply.github.com>
2022-07-06 17:49:51 +0530

    Time: 1 ms (90.61%), Space: 42.8 MB (82.84%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,30 +1,33 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int max = 0;
+        int min = Integer.MAX_VALUE;
+        int swap = 0;
         int idx = 0;
-        for(int i = nums.length - 1; i > 0; i--){
-            if(nums[i - 1] < nums[i]){
-                max = nums[i - 1];
-                idx = i;
+        int minidx = 0;
+        int i = 0;
+        for(i = nums.length - 1; i > 0; i--){
+            if(nums[i] > nums[i - 1]){
+                idx = i - 1; //1
                 break;
             }
         }
-        if(idx == 0){
+        if(i == 0){
             Arrays.sort(nums);
             return;
         }
-        int smallest = nums[idx];
-        int j = 0;
-        for(int i = idx; i < nums.length; i++){
-            if(nums[i] > max && nums[i] <= smallest){
-                smallest = nums[i];
-                j = i;
+        int number = nums[idx]; 
+        for(int j = nums.length - 1; j >= idx + 1; j--){
+            if(min > nums[j] && nums[j] > number){
+                min = nums[j];
+                minidx = j;
             }
         }
-        int temp = nums[idx - 1];
-        nums[idx - 1] = nums[j];
-        nums[j] = temp;
         
-        Arrays.sort(nums, idx, nums.length);
+        int temp = nums[idx];
+        nums[idx] = nums[minidx];
+        nums[minidx] = temp;
+        
+        Arrays.sort(nums, idx+1,nums.length);
+        return;
     }
 }
